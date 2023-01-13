a5049b70eacb4bb001588d5322d8a69ee97ed16d
Vishall001 <vshmandloi@gmail.com>
2023-01-13 17:59:58 +0530

    made some part of navbar

--- fitness_app/src/App.tsx
+++ fitness_app/src/App.tsx
@@ -1,10 +1,12 @@
 import Navbar from "@/component/navbar"
+import { useState } from "react"
 
 function App() {
+  const [selectedPage,setSelectedPage] =useState("home")
  
   return (
     <div className="bg-gray-20 ">
-    <Navbar/>
+    <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
     </div>
   )
 }
