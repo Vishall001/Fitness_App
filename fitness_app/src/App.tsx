11a5097b9874a50b9777d1e339cd39521c0285ea
Vishall001 <vshmandloi@gmail.com>
2023-01-23 13:07:38 +0530

    created the enum

--- fitness_app/src/App.tsx
+++ fitness_app/src/App.tsx
@@ -1,8 +1,10 @@
 import Navbar from "@/component/navbar"
 import { useState } from "react"
+import { SelectedPage } from "@/shared/types"
+
 
 function App() {
-  const [selectedPage,setSelectedPage] =useState("home")
+  const [selectedPage,setSelectedPage] =useState<SelectedPage>(SelectedPage.Home)
  
   return (
     <div className="bg-gray-20 ">
--- fitness_app/src/component/navbar/index.tsx
+++ fitness_app/src/component/navbar/index.tsx
@@ -2,9 +2,10 @@ import { useState } from "react"
 import {Bars3Icon,XMarkIcon} from "@heroicons/react/24/solid"
 import Logo from "@/assets/Logo.png"
 import Link from "./Link"
+import { SelectedPage } from "@/shared/types"
 type Props={
-  selectedPage:string;
-    setSelectedPage :(value:string) =>void
+  selectedPage:SelectedPage;
+    setSelectedPage :(value:SelectedPage) =>void
 }
 const Navbar =({selectedPage,setSelectedPage}:Props)=>{
     const flexBetween = "flex items-center justify-between";   
