b1ffda8378dbd71dda9edc6e32f70e56a0ca0f18
Vishall001 <vshmandloi@gmail.com>
2023-01-26 20:38:03 +0530

    Made the contact us section

--- fitness_app/src/component/contectUs/index.tsx
+++ fitness_app/src/component/contectUs/index.tsx
@@ -8,7 +8,7 @@ type  Props={
     setSelectedPage:(value:SelectedPage)=>void
 }
 const ContactUs = ({setSelectedPage}:Props) => {
-    const InputStyles =`mt-5 w-full rounded-lg bg-primary-300 px-5 py-3 placeholder-white`
+    const InputStyles =`mb-5 w-full rounded-lg bg-primary-300 px-5 py-3 placeholder-white`
     const {
         register,
         trigger,
@@ -117,6 +117,21 @@ const ContactUs = ({setSelectedPage}:Props) => {
                 SUBMIT
               </button>
              </form>
+            </motion.div>
+
+            <motion.div className='relative mt-16 basis-2/5 md:mt-0'
+             initial="hidden"
+             whileInView="visible"
+             viewport={{once:true,amount:0.5}}
+             transition={{delay:0.2,duration:0.5}}
+             variants={{
+                hidden :{opacity:0,y:60},
+                visible:{opacity:1,y:0}
+             }}
+            >
+           <div className='md:before:content-evolvetext w-full before:absolute before:-bottom-20 before:-right-10 before:z-[-1]'>
+            <img src={ContactUsPageGraphic} alt="Contact US" />
+           </div>
             </motion.div>
            </div>
         </motion.div>
